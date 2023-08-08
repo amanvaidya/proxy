90e4fe053224bfd98bc21ddda535485fa2038a10
amanvaidya <amanvaidya700@gmail.com>
2023-08-09 00:00:34 +0530

    Ratelimiter Implemented

--- src/main/java/com/hybridgenius/proxy/configuration/WebMvcConfig.java
+++ src/main/java/com/hybridgenius/proxy/configuration/WebMvcConfig.java
@@ -1,23 +1,23 @@
-//package com.hybridgenius.proxy.configuration;
-//
-//import com.hybridgenius.proxy.util.RateLimiterPerUserIP;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebMvcConfig implements WebMvcConfigurer {
-//
-//    private final RateLimiterPerUserIP rateLimiterPerUserIP;
-//
-//    public WebMvcConfig(RateLimiterPerUserIP rateLimiterPerUserIP) {
-//        this.rateLimiterPerUserIP = rateLimiterPerUserIP;
-//    }
-//
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(rateLimiterPerUserIP);
-//    }
-//
-//}
+package com.hybridgenius.proxy.configuration;
+
+import com.hybridgenius.proxy.util.RateLimiterPerUserIP;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final RateLimiterPerUserIP rateLimiterPerUserIP;
+
+    public WebMvcConfig(RateLimiterPerUserIP rateLimiterPerUserIP) {
+        this.rateLimiterPerUserIP = rateLimiterPerUserIP;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimiterPerUserIP);
+    }
+
+}
