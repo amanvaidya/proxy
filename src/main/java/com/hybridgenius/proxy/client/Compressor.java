90ed9a757e972f4fa3a16d0f007d3c9791f0557b
amanvaidya <amanvaidya700@gmail.com>
2023-08-09 23:16:52 +0530

    Feign client implemented

--- src/main/java/com/hybridgenius/proxy/client/Compressor.java
+++ src/main/java/com/hybridgenius/proxy/client/Compressor.java
@@ -2,11 +2,12 @@ package com.hybridgenius.proxy.client;
 
 
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
+@FeignClient(name = "compressor-proxy", url = "compressor")
 public interface Compressor {
 
     @PostMapping("/compress")
--- src/main/java/com/hybridgenius/proxy/client/UrlShortener.java
+++ src/main/java/com/hybridgenius/proxy/client/UrlShortener.java
@@ -2,12 +2,12 @@ package com.hybridgenius.proxy.client;
 
 
 import com.hybridgenius.proxy.dto.URLShortenerDto;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 
-
-
+@FeignClient(name = "url-shortener-proxy", url = "urlShortener")
 public interface UrlShortener {
 
     @PostMapping("/encode")
